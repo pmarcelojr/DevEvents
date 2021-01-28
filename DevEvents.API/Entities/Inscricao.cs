f80be12f8f00436183a49be69b86e660331a4866
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-01-27 21:39:24 -0300

    Created entities Inscricao

---
+++ DevEvents.API/Entities/Inscricao.cs
@@ -0,0 +1,9 @@
+namespace DevEvents.API.Entities
+{
+    public class Inscricao
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public int IdEvento { get; set; }
+    }
+}
